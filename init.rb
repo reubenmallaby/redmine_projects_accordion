eb13aca356a78afa9b269c6193ce8d4423a5f7bc
Reuben Mallaby <reuben@mallaby.me>
2012-11-08 14:37:04 +0100

    Update init.rb for jQuery version

--- init.rb
+++ init.rb
@@ -1,9 +1,9 @@
 Redmine::Plugin.register :redmine_projects_accordion do
   name 'Redmine Projects Accordion plugin'
   author 'Reuben Mallaby'
-  description 'This is a Redmine plugin which will turn the projects page into an accordion view using Accordion js'
-  version '0.0.2'
-  url 'http://isic.he-arc.ch'
+  description 'This is a Redmine plugin which will turn the projects page into an accordion view using jQuery UI accordion code'
+  version '0.1.0'
+  url 'http://reuben.mallaby.me'
   author_url 'http://isic.he-arc.ch/isic-galerie-institut-equipe/reuben-mallaby'
 end
 
@@ -11,8 +11,6 @@ class RedmineProjectsAccordionListener < Redmine::Hook::ViewListener
 
   # Adds javascript and stylesheet tags
   def view_layouts_base_html_head(context)
-    javascript_include_tag('accordion', :plugin => :redmine_projects_accordion) +
-    javascript_include_tag('redmine_projects_accordion', :plugin => :redmine_projects_accordion) +
     stylesheet_link_tag('redmine_projects_accordion', :plugin => :redmine_projects_accordion)
   end
   
